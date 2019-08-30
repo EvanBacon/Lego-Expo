eb1e0ef6ca7c2ab1f2a09491233cd4420b1a2dec
Evan Bacon <baconbrix@gmail.com>
2019-08-30 01:17:02 -0700

    Updated brand

---
+++ constants/LocalModels.js
@@ -0,0 +1,132 @@
+export default {
+  Architecture: {
+    '21002 - Empire State Building.mpd':
+      'Architecture/21002 - Empire State Building.mpd',
+    '21004 - Soloman R. Guggenheim Museum.mpd':
+      'Architecture/21004 - Soloman R. Guggenheim Museum.mpd',
+    '21005 - Falling Water.mpd': 'Architecture/21005 - Falling Water.mpd',
+    '21008 - Burj Kalifa.mpd': 'Architecture/21008 - Burj Kalifa.mpd',
+    '21009 - Farnsworth House.mpd': 'Architecture/21009 - Farnsworth House.mpd',
+    '21010 - Robie House.mpd': 'Architecture/21010 - Robie House.mpd',
+    '21011 - Brandenburg Gate.mpd': 'Architecture/21011 - Brandenburg Gate.mpd',
+    '21013 - Big Ben.mpd': 'Architecture/21013 - Big Ben.mpd',
+  },
+  Creator: {
+    '10214 - Tower Bridge.mpd': 'Creator/10214 - Tower Bridge.mpd',
+    '4838 - Mini Vehicles - Carrier and Car.mpd':
+      'Creator/4838 - Mini Vehicles - Carrier and Car.mpd',
+  },
+  'Modular buildings': {
+    '10182 - Cafe Corner.mpd': 'Modular buildings/10182 - Cafe Corner.mpd',
+    '10185 - Green Grocer.mpd': 'Modular buildings/10185 - Green Grocer.mpd',
+    '10190 - Market street.mpd': 'Modular buildings/10190 - Market street.mpd',
+    '10197 - Fire Brigade.mpd': 'Modular buildings/10197 - Fire Brigade.mpd',
+  },
+  Space: {
+    '6780 - Light & Sound XT Starship.mpd':
+      'Space/6780 - Light & Sound XT Starship.mpd',
+    '6801 - Rocket Sled.mpd': 'Space/6801 - Rocket Sled.mpd',
+    '6821 - Shovel Buggy.mpd': 'Space/6821 - Shovel Buggy.mpd',
+    '6822 - Space Shuttle.mpd': 'Space/6822 - Space Shuttle.mpd',
+    '6827 - Strata Scooter.mpd': 'Space/6827 - Strata Scooter.mpd',
+    '6841 - Mineral Detector.mpd': 'Space/6841 - Mineral Detector.mpd',
+    '6842 - Shuttle Craft.mpd': 'Space/6842 - Shuttle Craft.mpd',
+    '6861 - X-1 Patrol Craft.mpd': 'Space/6861 - X-1 Patrol Craft.mpd',
+    '6870 - Space Probe Launcher.mpd': 'Space/6870 - Space Probe Launcher.mpd',
+    '6875 - Hovercraft.mpd': 'Space/6875 - Hovercraft.mpd',
+    '6891 - Gamma-V Laser Craft.mpd': 'Space/6891 - Gamma-V Laser Craft.mpd',
+    '6901 - Mobile Lab.mpd': 'Space/6901 - Mobile Lab.mpd',
+    '6927 - All-Terrain Vehicle.mpd': 'Space/6927 - All-Terrain Vehicle.mpd',
+    '6929 - Starfleet Voyager.mpd': 'Space/6929 - Starfleet Voyager.mpd',
+    '6933 - Spectral Starguider.mpd': 'Space/6933 - Spectral Starguider.mpd',
+    '6951 - Robot Command Center.mpd': 'Space/6951 - Robot Command Center.mpd',
+    '6970 - Beta-1 Command Base.mpd': 'Space/6970 - Beta-1 Command Base.mpd',
+    '885 - Space Scooter.mpd': 'Space/885 - Space Scooter.mpd',
+    '886 - Space Buggy.mpd': 'Space/886 - Space Buggy.mpd',
+    '889 - Radar Truck.mpd': 'Space/889 - Radar Truck.mpd',
+    '891 - Two-Man Scooter.mpd': 'Space/891 - Two-Man Scooter.mpd',
+    '894 - Mobile Ground Tracking Station.mpd':
+      'Space/894 - Mobile Ground Tracking Station.mpd',
+    '897 - Rocket Launcher.mpd': 'Space/897 - Rocket Launcher.mpd',
+    '918 - Space Transport.mpd': 'Space/918 - Space Transport.mpd',
+    '920 - Alpha-1 Rocket Base.mpd': 'Space/920 - Alpha-1 Rocket Base.mpd',
+    '924 - Space Cruiser.mpd': 'Space/924 - Space Cruiser.mpd',
+    '926 - Space Command Center.mpd': 'Space/926 - Space Command Center.mpd',
+    '928 - Galaxy Explorer.mpd': 'Space/928 - Galaxy Explorer.mpd',
+  },
+  Technic: {
+    '8464 - Front End Loader.mpd': 'Technic/8464 - Front End Loader.mpd',
+  },
+  'Star Wars': {
+    '20019 - Slave I.mpd': 'Star Wars/20019 - Slave I.mpd',
+    '20021 - Bounty Hunter Assault Gunship.mpd':
+      'Star Wars/20021 - Bounty Hunter Assault Gunship.mpd',
+    '30050 - Republic Attack Shuttle.mpd':
+      'Star Wars/30050 - Republic Attack Shuttle.mpd',
+    '30051 - X-Wing Fighter.mpd': 'Star Wars/30051 - X-Wing Fighter.mpd',
+    '30052 - AAT.mpd': 'Star Wars/30052 - AAT.mpd',
+    '30053 - Republic Attack Cruiser.mpd':
+      'Star Wars/30053 - Republic Attack Cruiser.mpd',
+    '30054 - AT-ST.mpd': 'Star Wars/30054 - AT-ST.mpd',
+    '30055 - Vulture Droid.mpd': 'Star Wars/30055 - Vulture Droid.mpd',
+    '4484 - X-Wing Fighter & TIE Advanced.mpd':
+      'Star Wars/4484 - X-Wing Fighter & TIE Advanced.mpd',
+    '4484+4485+4485+4486 - TIE Bomber.mpd':
+      'Star Wars/4484+4485+4485+4486 - TIE Bomber.mpd',
+    '4485 - Anakins & Sebulbas Podracer.mpd':
+      'Star Wars/4485 - Anakins & Sebulbas Podracer.mpd',
+    '4486 - AT-ST & Snowspeeder.mpd':
+      'Star Wars/4486 - AT-ST & Snowspeeder.mpd',
+    '4487 - Jedi Starfighter & Slave I.mpd':
+      'Star Wars/4487 - Jedi Starfighter & Slave I.mpd',
+    '4488 - Millenium Falcon.mpd': 'Star Wars/4488 - Millenium Falcon.mpd',
+    '4488+4489+4490+4491 - Y-Wing.mpd':
+      'Star Wars/4488+4489+4490+4491 - Y-Wing.mpd',
+    '4489 - AT-AT.mpd': 'Star Wars/4489 - AT-AT.mpd',
+    '4490 - Republic Gunship.mpd': 'Star Wars/4490 - Republic Gunship.mpd',
+    '4491 - MTT.mpd': 'Star Wars/4491 - MTT.mpd',
+    '4492 - Star Destroyer.mpd': 'Star Wars/4492 - Star Destroyer.mpd',
+    '4493 - Sith Infiltrator.mpd': 'Star Wars/4493 - Sith Infiltrator.mpd',
+    '4494 - Imperial Shuttle.mpd': 'Star Wars/4494 - Imperial Shuttle.mpd',
+    '4495 - AT-TE.mpd': 'Star Wars/4495 - AT-TE.mpd',
+    '6964 - Boba Fetts Slave I.mpd': 'Star Wars/6964 - Boba Fetts Slave I.mpd',
+    '6965 - TIE Interceptor.mpd': 'Star Wars/6965 - TIE Interceptor.mpd',
+    '6966 - Jedi Starfighter.mpd': 'Star Wars/6966 - Jedi Starfighter.mpd',
+    '6967 - ARC Fighter.ldr': 'Star Wars/6967 - ARC Fighter.ldr',
+    '6967 - Main model.ldr': 'Star Wars/6967 - Main model.ldr',
+    '7101 - Lightsaber Duel.mpd': 'Star Wars/7101 - Lightsaber Duel.mpd',
+    '7110 - Landspeeder.mpd': 'Star Wars/7110 - Landspeeder.mpd',
+    '7111 - Droid Fighter.mpd': 'Star Wars/7111 - Droid Fighter.mpd',
+    '7121 - Naboo Swamp.mpd': 'Star Wars/7121 - Naboo Swamp.mpd',
+    '7128 - Speeder Bikes.mpd': 'Star Wars/7128 - Speeder Bikes.mpd',
+    '7130 - Snowspeeder.mpd': 'Star Wars/7130 - Snowspeeder.mpd',
+    "7131 - Anakin's Podracer.mpd": "Star Wars/7131 - Anakin's Podracer.mpd",
+    '7140 - X-Wing Fighter.mpd': 'Star Wars/7140 - X-Wing Fighter.mpd',
+    '7141 - Naboo Fighter.mpd': 'Star Wars/7141 - Naboo Fighter.mpd',
+    '7150 - TIE Fighter & Y-Wing.mpd':
+      'Star Wars/7150 - TIE Fighter & Y-Wing.mpd',
+    '8028 - TIE Fighter.mpd': 'Star Wars/8028 - TIE Fighter.mpd',
+    '8029 - Snowspeeder.mpd': 'Star Wars/8029 - Snowspeeder.mpd',
+    '8031 - V-19 Torrent.mpd': 'Star Wars/8031 - V-19 Torrent.mpd',
+    '8033 - General Grievous Starfighter.mpd':
+      'Star Wars/8033 - General Grievous Starfighter.mpd',
+  },
+  Town: {
+    '6608 - Tractor.mpd': 'Town/6608 - Tractor.mpd',
+  },
+  Train: {
+    '0112 - Locomotive with Motor.mpd':
+      'Train/0112 - Locomotive with Motor.mpd',
+  },
+  evo: {
+    'MillenniumFalcon_75193.ldr': 'evo/MillenniumFalcon_75193.ldr',
+    'modelHoverpod1.mpd': 'evo/modelHoverpod1.mpd',
+    'modelHoverpod2.mpd': 'evo/modelHoverpod2.mpd',
+    'modelHoverpod3.mpd': 'evo/modelHoverpod3.mpd',
+    'modelTest.mpd': 'evo/modelTest.mpd',
+    'modelViper.ldr': 'evo/modelViper.ldr',
+    'modelbuggy.mpd': 'evo/modelbuggy.mpd',
+    'tie.ldr': 'evo/tie.ldr',
+    'xwing.ldr': 'evo/xwing.ldr',
+  },
+};
